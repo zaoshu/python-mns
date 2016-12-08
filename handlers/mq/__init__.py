936cabcf3863541183f3b26dffa4d6b313a2aaa3
wzyplus <exmail.wang@gmail.com>
2016-12-08 14:22:07 +0800

    empty repo

--- handlers/mq/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-__author__ = 'wzy'
-__date__ = '2015-09-01 15:23'
-
-
-from MessageHandler import MessageHandler
-# from DelayTask import DelayTask
--- settings/mq.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-__author__ = 'wzy'
-__date__ = '2015-09-01 16:00'
-
-
-MQ_SPIDER_SERVICE = 'MyQueue'
